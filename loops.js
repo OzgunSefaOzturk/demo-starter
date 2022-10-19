16a0b35f8e0829924c2dd95514b2fc64a958846e
OzgunSefaOzturk <ozgunsefaa@gmail.com>
2022-10-19 12:51:11 +0300

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,25 @@
+// dinamik olacak
+
+let krediler = ["ihtiyaç", "taşıt", "araç"];
+
+for (let i = 0; i < krediler.length; i++) {
+  console.log(krediler[i]);
+}
+
+krediler.forEach((kredi) => {
+  console.log("Foreach: ", kredi);
+});
+
+let urunler = [
+  { fiyat: 100, ad: "ürün 1", discount: "true", rate: 15 },
+  { fiyat: 150, ad: "ürün 2", discount: "false", rate: 0 },
+  { fiyat: 200, ad: "ürün 3", discount: "true", rate: 2.5 },
+];
+
+urunler.forEach((urun) => {
+  if (urun.discount === true) {
+    urun.fiyat -= (urun.fiyat * urun.rate) / 100;
+  }
+  if (urun.fiyat) {
+  }
+});
